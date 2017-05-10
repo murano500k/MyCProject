467fdc6cc88f869f275d20380feb77434c3e63f2
Artem <murano500k@gmail.com>
2017-05-10 14:09:57 +0300

    Chapter 2 finished

---
+++ chapter_2/exercise_2-9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int bitcount (unsigned x)
+
+{
+	int	b;
+
+
+	for (b = 0; x != 0; x &= (x - 1))
+
+		b++;
+	
+	return b;
+}
+
+int main ()
+
+{
+	int	x = 0xff;
+
+	printf("bitcount(0x%x) = %d\n", x, bitcount(x));
+	
+	return 0;
+}
